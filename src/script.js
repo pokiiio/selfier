c0ec96629b75f760197581076d662e7b59870a57
pokio <32353873+pokiiio@users.noreply.github.com>
2019-03-29 12:10:49 +0900

    Update some codes.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,49 @@
 var width = 1280;
 var height = 720;
+
 var video = document.getElementById('camera');
 var canvas = document.getElementById('canvas');
+
 var lastPictureJpeg = null;
 var lastPictureDateString = '';
+
 var ipc = require('electron').ipcRenderer;
+var EventEmitter = require('events').EventEmitter;
+var event = new EventEmitter();
+
+video.onclick = function () {
+    event.emit('pause');
+}
+
+canvas.onclick = function () {
+    event.emit('pause');
+};
 
 canvas.style.display = 'none';
+
+ipc.on('save', function (ev, arg) {
+    event.emit('save');
+});
+
+ipc.on('pause', function (ev, arg) {
+    event.emit('pause');
+});
+
+event.on('save', function (ev, arg) {
+    if (canvas.style.display === 'none') {
+        takePicture();
+    }
+
+    saveDataUrl();
+});
+
+event.on('pause', function (ev, arg) {
+    if (canvas.style.display === 'none') {
+        takePicture();
+    } else {
+        canvas.style.display = 'none';
+    }
+});
 
 window.URL = window.URL || window.webkitURL;
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -22,41 +59,29 @@ navigator.getUserMedia({
 }, function (stream) {
     video.srcObject = stream;
 }, function (error) {
-    console.log('エラー!', error);
+    console.log('error : ', error);
 });
 
-ipc.on('save', function (event, arg) {
-    if (canvas.style.display === 'none') {
-        takePicture();
-    }
-
-    savePicture();
-});
-
-ipc.on('pause', function (event, arg) {
-    if (canvas.style.display === 'none') {
-        takePicture();
-    } else {
-        canvas.style.display = 'none';
-    }
-});
-
-var takePicture = function () {
+function takePicture() {
     canvas.style.display = 'block';
-    createJpegData();
-    displaySavedPicture();
+    takeLiveViewSnapShot();
+    saveCanvasAsDataUrl();
+    resizeCanvasToFitScreen();
 }
 
-function createJpegData() {
+function takeLiveViewSnapShot() {
     var ctx = canvas.getContext('2d');
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
     ctx.drawImage(video, 0, 0, width, height);
+}
+
+function saveCanvasAsDataUrl() {
     lastPictureJpeg = canvas.toDataURL('image/jpeg');
     lastPictureDateString = new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).toISOString().replace(/([\-T:.Z])/g, '');
 }
 
-function displaySavedPicture() {
+function resizeCanvasToFitScreen() {
     var videoWidth = video.offsetWidth;
     var videoHeight = video.offsetHeight;
 
@@ -73,12 +98,9 @@ function displaySavedPicture() {
 
     canvas.style.width = canvasWidth + 'px';
     canvas.style.height = canvasHeight + 'px';
-    canvas.onclick = function () {
-        canvas.style.display = 'none';
-    };
 }
 
-function savePicture() {
+function saveDataUrl() {
     var a = document.createElement('a');
     a.href = lastPictureJpeg;
     a.download = 'image_' + lastPictureDateString + '.jpg';
